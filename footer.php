a9b3d2ba3fd6cbb247334cb98ed74640b551db90
TonyHe <he@holptech.com>
2019-02-18 21:05:53 +0800

    Add authors link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
                     <footer class="footer reveal" style="<?php if(wp_is_mobile()) echo 'display:none;' ?>">
-                        <p>Copyright © 2018 <?php echo get_bloginfo('name'); ?> · Theme Tony | Designed By TonyHe</p>
+                        <p>Copyright © 2018 <?php echo get_bloginfo('name'); ?> · Theme Tony | Designed By <a href="https://www.ouorz.com">TonyHe</a></p>
                     </footer>
                 </div>
             </div>
