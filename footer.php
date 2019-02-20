574199d9ebed7e743ac072977e95970283b8b107
TonyHe <he@holptech.com>
2019-02-20 09:57:12 +0800

    Fixed Bootstrap js Bug

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
                     <footer class="footer reveal" style="<?php if(wp_is_mobile()) echo 'display:none;' ?>">
                         <p>Copyright © 2018 <?php echo get_bloginfo('name'); ?> · Theme Tony | Designed By <a href="https://www.ouorz.com">TonyHe</a></p>
                     </footer>
+                    <script src="https://static.ouorz.com/bootstrap.min.js"></script>
                 </div>
             </div>
         </main>
