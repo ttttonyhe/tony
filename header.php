117aa050e4240cada7773ee1b778eac298d6d980
TonyHe <he@holptech.com>
2019-03-09 21:41:19 +0800

    New feature - Search

--- header.php
+++ header.php
@@ -32,10 +32,12 @@
                 <div class="header-div1">
         	<a href="<?php echo site_url() ?>" style="display: inline-block;"><img src="<?php echo get_option('king_logo') ?>"></a>
 <a href="<?php echo site_url() ?>/feed" style="display: inline-block;margin-top: 12px;margin-left: 15px;"><button type="button" class="btn btn-light" style="letter-spacing: 1px;font-weight: 500;">RSS订阅</button></a>
-        </div>
+<a style="margin-top: 12px;margin-left: 12px;"><button onclick="open_search();" type="button" class="btn btn-light" style="letter-spacing: 1px;font-weight: 500;">博客搜索</button></a>
+</div>
                 <?php }else{ ?>
         <div class="header-div1-1">
         	<a href="<?php echo site_url() ?>"><img src="<?php echo get_option('king_logo'); ?>"></a>
+        	<a style="margin-top: 12px;margin-left: 12px;"><button onclick="open_search();" type="button" class="btn btn-light" style="letter-spacing: 1px;font-weight: 500;">博客搜索</button></a>
         </div>
         <?php } ?>
         <div class="header-div2">
@@ -75,6 +77,17 @@
         }
       });
       
+      var open_search = function(){
+          $('#search_form').attr('class','search_form_play');
+          $('.search-bg-b').attr('style','display:block');
+          $('#search-div').attr('style','display:block');
+      }
+      var close_search = function(){
+          $('#search_form').attr('class','search_form_dis');
+          $('.search-bg-b').attr('style','display:none');
+          $('#search-div').attr('style','display:none');
+      }
+      
     </script>
             
             
@@ -103,6 +116,70 @@ $("#jv-loadingbar").animate({width:"20%"},100);
 $("#jv-loadingbar").animate({width:"100%"},100,function(){
 $("#jv-loadingbar").fadeOut(1000,function(){$("#jv-loadingbar").css("width","0");});
 });
+</script>
+
+<div id="search_form" class="search_form_dis">
+<div class="search-bg-b"></div>
+<div class="search-bg" id="search-div">
+    <div class="search-div1">
+        <h3>搜索内容<button type="button" class="btn btn-primary" style="font-weight: 600;padding: 4px 14px;font-size: .9rem;margin-top: 6px;margin-left: 10px;float: right;" onclick="close_search();">关闭</button></h3>
+        <p>从博客全部内容中搜索所需</p>
+        <input class="uk-input" type="text" placeholder="请输入搜索内容并回车Enter..." v-on:keyup.enter="search_query" v-model.trim="search_key">
+    </div>
+    <div class="search-div2">
+        <ul v-if="search_loading">
+            <template v-if="search_content.length !== 0">
+                <li v-for="search in search_content">
+                    <a :href="search.link">
+                        <h4 v-html="search.title.rendered"></h4>
+                        <p v-html="search.post_excerpt.four"></p>
+                    </a>
+                </li>
+            </template>
+            <template v-else>
+                <li>
+                    <h4 style="color:#777">无匹配文章</h4>
+                    <p>请尝试更换搜索词再试试吧...</p>
+                </li>
+            </template>
+        </ul>
+        <ul v-if="loading_ph">
+            <ul class="uk-animation-slide-bottom-small">
+                <li class="search-line" style="padding: 30px;background: #f1f2f3;border: none;"></li>
+                <li class="search-line" style="padding: 30px;background: #f1f2f3;border: none;"></li> 
+                <li class="search-line" style="padding: 30px;background: #f1f2f3;border: none;"></li>
+            </ul>
+    </div>
+</div>
+</div>
+
+<script>
+    var search_vue = new Vue({
+        el: '#search-div',
+        data(){
+            return {
+                search_content : null,
+                search_key : null,
+                search_loading : false,
+                loading_ph : true,
+                search_open : false
+            }
+        },
+        methods : {
+            search_query : function(){
+                this.search_loading = false;
+                this.loading_ph = true;
+                
+                var s = this.search_key;
+                axios.get('<?php echo site_url() ?>/wp-json/wp/v2/posts?search='+ s +'<?php if(get_option('king_index_exclude')) echo '&categories_exclude='.get_option('king_index_exclude'); ?>')
+                 .then(response => {
+                     this.search_content = response.data;
+                     this.loading_ph = false;
+                     this.search_loading = true;
+                 })
+            }
+        }
+    })
 </script>
         <main role="main">
             <div class="grid grid-centered" style="<?php if(!is_single() && !is_page()) echo 'max-width: 660px;padding: 0px 20px;margin-top: 80px;'; ?>">
